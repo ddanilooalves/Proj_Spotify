faf77a109c4c9dd8160eb2f3b41587450f34d1df
ddanilooalves <daniloalves0512@gmail.com>
2022-06-27 21:24:51 -0300

    chora: correction docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @ApiTags('status')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  /**
+   * Recebe uma requisição GET e retorna um objeto de status
+   * da aplicação com a URL de documentação
+   * @param req Objeto de Request do Express
+   * @returns Objeto de status da aplicação
+   */
   @Get()
   @ApiOperation({
     summary: 'Visualizar status da aplicação'
   })
-  getStatus(): string {
-    return this.appService.getStatus('http://localhost:3000');
+  getStatus(@Req() req: Request) {
+    const baseUrl = req.protocol + '://' + req.get('host');
+    return this.appService.getStatus(baseUrl);
   }
 }
 
